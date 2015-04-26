e532a2f40e505d53764025eecea10f2b6745d0b6
Emma Marriott <mekmarriott@gmail.com>
2015-04-26 15:39:17 -0700

    added pages, navbar, and working links

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Comply' });
+});
+
+/* GET progress page. */
+router.get('/progress', function(req, res, next) {
+  res.render('progress', { title: 'Progress' });
+});
+
+/* GET medications page. */
+router.get('/medications', function(req, res, next) {
+  res.render('medications', { title: 'Medications' });
+});
+
+/* GET upload page. */
+router.get('/upload', function(req, res, next) {
+  res.render('upload', { title: 'Upload' });
+});
+
+/* GET message inbox page. */
+router.get('/messages', function(req, res, next) {
+  res.render('messages', { title: 'Messages' });
+});
+
+/* GET reminders page. */
+router.get('/reminders', function(req, res, next) {
+  res.render('reminders', { title: 'Reminders' });
+});
+
+/* GET settings page. */
+router.get('/settings', function(req, res, next) {
+  res.render('settings', { title: 'Settings' });
 });
 
 module.exports = router;
